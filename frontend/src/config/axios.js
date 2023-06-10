bbd1eed4704597e725bc2245435daa99bc7e00a0
Dhamareshwarakumar Gandikota <damareshwarakumar@gmail.com>
2023-06-10 16:16:43 +0530

    rem front

--- frontend/src/config/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-function setAxiosDefaults() {
-    console.log('[config][axios] Setting Axios defaults')
-    // axios.defaults.baseURL = 'http://localhost:5555/';
-    axios.defaults.baseURL = 'https://todov2-production.up.railway.app/';
-}
-
-export { setAxiosDefaults };
