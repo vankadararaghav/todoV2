c4a88c0c424e4bec1d9bf514cbfce7f45e9d3042
Dhamareshwarakumar Gandikota <damareshwarakumar@gmail.com>
2023-06-10 16:14:31 +0530

    axios defaults removed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,24 +1,24 @@
 import React from "react";
-import  ReactDOM from "react-dom/client";
-import {BrowserRouter as Router, Routes,Route} from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./screens/Signup"
-import Login  from "./screens/Login"
-import App  from "./App";
-import { setAxiosDefaults } from './config/axios';
+import Login from "./screens/Login"
+import App from "./App";
+// import { setAxiosDefaults } from './config/axios';
 
-setAxiosDefaults();
+// setAxiosDefaults();
 
 const element = document.getElementById("root");
 
-const root  = ReactDOM.createRoot(element);
+const root = ReactDOM.createRoot(element);
 
 root.render(
-     <Router>
+    <Router>
         <Routes>
-            <Route path="/" element={<Signup/>} />
-            <Route path="/App" element={<App/>} />
-            <Route path="/login" element={<Login/>} />
-            <Route path="/signup" element={<Signup/>} />
+            <Route path="/" element={<Signup />} />
+            <Route path="/App" element={<App />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
         </Routes>
-     </Router>
+    </Router>
 )
