7cae79446d369649a7cb6c382943a6c69f1d697c
Dhamareshwarakumar Gandikota <damareshwarakumar@gmail.com>
2023-06-10 16:06:30 +0530

    axios base url set

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 function setAxiosDefaults() {
     console.log('[config][axios] Setting Axios defaults')
-    axios.defaults.baseURL = 'http://localhost:5555/';
-    // axios.defaults.baseURL = 'https://basictodo.up.railway.app/';
+    // axios.defaults.baseURL = 'http://localhost:5555/';
+    axios.defaults.baseURL = 'https://todov2-production.up.railway.app/';
 }
 
 export { setAxiosDefaults };
